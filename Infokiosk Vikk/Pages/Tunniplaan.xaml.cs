a8f5b1492c8ebf0974ddf49f79a898c50b54ed0a
Karl Fedjuk <karl.fedjuk>
2017-02-09 13:53:52 +0200

    4ljas commit

--- Infokiosk Vikk/Pages/Tunniplaan.xaml.cs
+++ Infokiosk Vikk/Pages/Tunniplaan.xaml.cs
@@ -12,6 +12,9 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Windows.Web.Http;
+using Windows.Data.Json;
+
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -25,6 +28,27 @@ namespace Infokiosk_Vikk
         public Tunniplaan()
         {
             this.InitializeComponent();
+            LaeVeebiandmed();
+        }
+
+        public async void LaeVeebiandmed()
+        {
+            var veebileht = new HttpClient();
+            HttpResponseMessage vastus = await veebileht.GetAsync(new Uri("https://vikk.siseveeb.ee/veebilehe_andmed/tunniplaan?nadal=06.02.2017&nimekiri=grupp"));
+            var sisu = await vastus.Content.ReadAsStringAsync();
+            //    SisuBlock.Text = sisu;
+            JsonArray root = JsonArray.Parse(sisu).GetArray();
+        }
+        public class Grupp
+        {
+            public string id { get; set; }
+            public string nimi { get; set; }
+        }
+
+        public class RootObject
+        {
+            public string nadal { get; set; }
+            public List<Grupp> grupp { get; set; }
         }
     }
 }
